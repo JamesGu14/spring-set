123b2ee3a8d6280179aa1f5c0a38d02ff9d1a223
James Gu <james_gu14@hotmail.com>
2016-09-20 23:18:01 +0800

    01hibernate

--- 01hibernate/src/main/java/com/senso/hibernate/App.java
+++ 01hibernate/src/main/java/com/senso/hibernate/App.java
@@ -1,13 +1,47 @@
 package com.senso.hibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.senso.pojo.SystemUser;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	/**
+	 * 1. 新建Configuration对象
+	 * 2. 通过Configuration创建SessionFactory对象
+	 * 3. 通过SessionFactory得到Session
+	 * 4. 通过session对象得到Transaction对象
+	 * 5. 保存数据
+	 * 6. 提交事务
+	 * 7. 关闭Session
+	 */
+	
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration().configure();
+		
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		Transaction transaction = session.beginTransaction();
+		
+		SystemUser systemUser = new SystemUser();
+		systemUser.setEmailaddress("test@gmail.com");
+		systemUser.setFirstname("James");
+		systemUser.setLastname("Gu");
+		systemUser.setPassword("123");
+		systemUser.setUsername("james3299");
+		systemUser.setUserrole(1);
+		session.save(systemUser);
+		
+		transaction.commit();
+		session.close();
+	}
 }
---
+++ 01hibernate/src/main/java/com/senso/pojo/SystemUser.java
@@ -0,0 +1,60 @@
+package com.senso.pojo;
+
+public class SystemUser {
+	private int id;
+	private String username;
+	private String emailaddress;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private int userrole;
+	private String description;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailaddress() {
+		return emailaddress;
+	}
+	public void setEmailaddress(String emailaddress) {
+		this.emailaddress = emailaddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public int getUserrole() {
+		return userrole;
+	}
+	public void setUserrole(int userrole) {
+		this.userrole = userrole;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
